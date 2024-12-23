12b77101372d1c4defef44adce1c1dbbbff9b806
Mohamed Shakin <shakincc02@gmail.com>
2024-12-23 10:41:33 +0400

    Code for navigation bar

---
+++ Front-End/watch-project/src/components/Menu.jsx
@@ -0,0 +1,21 @@
+function Menu() {
+    return (
+        <header>
+            <nav>
+                <ul className="Logo">
+                    <li><a href="" title="Logo"><img src="/images/G-shock logo.png" alt=""/></a></li>
+                </ul>
+
+                <ul className="Menus">
+                    <li><a href="">Home</a></li>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Features</a></li>
+                    <li><a href="">Contact</a></li>
+
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Menu; 
