aa4eb9b5886a3ead7277c9030edb7a253c59c929
Mohamed Shakin <shakincc02@gmail.com>
2024-12-23 10:40:32 +0400

    Code for about section

---
+++ Front-End/watch-project/src/components/About.jsx
@@ -0,0 +1,15 @@
+function About() {
+    return (
+        <>
+            <div className="about">
+                <img src="/images/about-left.png" alt="" />
+                <p>Defy limits and embrace rugged resilience with G-Shock timepieces. 
+                    Built for extreme conditions, these iconic watches blend cutting-edge tech with unmatched durability.
+                     From outdoor adventures to urban streetwear, G-Shock lets you make a bold statement while redefining toughness.</p>
+                <img src="/images/about-right.png" alt="" />
+            </div>
+        </>
+    );
+}
+
+export default About; 
