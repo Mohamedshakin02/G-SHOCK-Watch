b7f96aeacf55e4f4539ef3508d9b33241193f3d1
Mohamed Shakin <shakincc02@gmail.com>
2024-12-23 10:41:13 +0400

    Code for Introduction section

---
+++ Front-End/watch-project/src/components/Intro.jsx
@@ -0,0 +1,21 @@
+function Intro() {
+    return (
+        <>
+            <div className="intro">
+                <div className="watch_img">
+                    <img src="/images/G-shock watch.png" alt="" />
+                </div>
+
+                <div className="watch-detail">
+                    <p className="collab">Charles Darwin Foundation collaboration model</p>
+                    <p className="watch-title">GA-B2100CD-1A7</p>
+                    <p className="watch-desc">Show your love of the Galápagos and the Charles Darwin Foundation with a G-SHOCK inspired by animals living in these gorgeous islands.</p>
+                    <button>Discover</button>
+                    <button className="Cart">Add to Cart</button>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Intro; 
