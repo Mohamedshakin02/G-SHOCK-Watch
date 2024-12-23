2a8e0dba592e1fb30ebe5efd940637d76bc904e9
Mohamed Shakin <shakincc02@gmail.com>
2024-12-23 10:40:51 +0400

    Code for Features section

---
+++ Front-End/watch-project/src/components/Features.jsx
@@ -0,0 +1,46 @@
+function Features() {
+    return (
+        <>
+            <div className="feature-section">
+                <div className="feature-intro">
+                    <h1>Discover What Makes It Unique</h1>
+                </div>
+
+                <div className="features-container">
+                    <div className="feature feature-1">
+                        <img src="/images/collab.png" alt="" />
+                        <p>Collaboration model</p>
+                    </div>
+
+                    <div className="feature feature-2">
+                       <img src="/images/shock.png" alt="" />
+                       <p>Shock resistant</p>
+                    </div>
+
+                    <div className="feature feature-3">
+                        <img src="/images/water-resistant.png" alt="" />
+                        <p>20 bar water resistant</p>
+                    </div>
+
+                    <div className="feature feature-4">
+                        <img src="/images/your-time.png" alt="" />
+                       <p>Keeps yourself in time</p>
+                    </div>
+
+                   <div className="feature feature-5">
+                        <img src="/images/solor.png" alt="" />
+                        <p>Stable solar power</p>
+                    </div>
+
+                    <div className="feature feature-6">
+                        <img src="/images/bluetooth.png" alt="" />
+                        <p>Easy to operate via smartphone</p>
+                    </div>
+
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Features; 
