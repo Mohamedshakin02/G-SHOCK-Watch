b49f9f667771453e1a4e519e8a5682f486b68a0e
Mohamed Shakin <shakincc02@gmail.com>
2024-12-24 11:30:22 +0400

    Added login and sign up button

--- Front-End/watch-project/src/components/Menu.jsx
+++ Front-End/watch-project/src/components/Menu.jsx
@@ -1,16 +1,23 @@
 function Menu() {
     return (
         <header>
+
+            <ul className="Logo">
+                <li><a href="" title="Logo"><img src="/images/G-shock logo.png" alt="" /></a></li>
+            </ul>
+
             <nav>
-                <ul className="Logo">
-                    <li><a href="" title="Logo"><img src="/images/G-shock logo.png" alt=""/></a></li>
-                </ul>
 
                 <ul className="Menus">
                     <li><a href="">Home</a></li>
                     <li><a href="">About</a></li>
                     <li><a href="">Features</a></li>
                     <li><a href="">Contact</a></li>
+
+                    <ul className="buttons">
+                    <li><a href=""><button className="button button-1">Login</button></a></li>
+                    <li><a href=""><button className="button button-2">Sign Up</button></a></li>
+                    </ul>
 
                 </ul>
             </nav>
