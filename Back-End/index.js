3a70070511c0c1dde6772bc6794247749a3e913d
Mohamed Shakin <shakincc02@gmail.com>
2024-12-25 20:37:41 +0400

    added code in backend

--- Back-End/index.js
+++ Back-End/index.js
@@ -8,7 +8,8 @@ app.use(express.json())
 app.use(cors(
     {
         origin: ["https://g-shock.vercel.app"],
-        methods:["POST","GET"],
+        methods:["POST","GET", 'PUT', 'DELETE', 'OPTIONS'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
         credentials: true
     }
 ))
