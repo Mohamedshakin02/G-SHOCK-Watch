7df00186ac226de2c4da93897e928d5afbb55636
Mohamed Shakin <shakincc02@gmail.com>
2024-12-25 13:34:37 +0400

    Updated the component in main.jsx

--- Front-End/watch-project/src/main.jsx
+++ Front-End/watch-project/src/main.jsx
@@ -1,24 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Menu from './components/Menu'
 import './stylesheet/style.css'
-import Intro from './components/Intro'
-import About from './components/About'
-import Features from './components/Features'
-import Collections from './components/collections'
-import Footer from './components/Footer'
+import Menu from './components/Menu'
 // import './index.css'
 // import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Menu/>
-    <main>
-      <Intro/>
-      <About/>
-      <Features/>
-      <Collections/>
-    </main>
-    <Footer/>
   </StrictMode>,
 )
