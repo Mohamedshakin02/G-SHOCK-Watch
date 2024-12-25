04204281e60b7dbcf2d143a869b6df98c230a8a0
Mohamed Shakin <shakincc02@gmail.com>
2024-12-25 13:28:33 +0400

    Changed anchor tag to link tag in navigation bar

--- Front-End/watch-project/src/components/Menu.jsx
+++ Front-End/watch-project/src/components/Menu.jsx
@@ -1,28 +1,55 @@
+import React, { useState } from "react"
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import Login from './Login';
+import Signup from './Sign-up';
+
 function Menu() {
+
+    const [menuOpen, setMenuOpen] = useState(false)
     return (
-        <header>
-
-            <ul className="Logo">
-                <li><a href="" title="Logo"><img src="/images/G-shock logo.png" alt="" /></a></li>
-            </ul>
-
-            <nav>
-
-                <ul className="Menus">
-                    <li><a href="">Home</a></li>
-                    <li><a href="">About</a></li>
-                    <li><a href="">Features</a></li>
-                    <li><a href="">Contact</a></li>
-
-                    <ul className="buttons">
-                    <li><a href=""><button className="button button-1">Login</button></a></li>
-                    <li><a href=""><button className="button button-2">Sign Up</button></a></li>
+        <BrowserRouter>
+            <>
+                <header>
+                    <ul className="Logo">
+                        <li><Link to="/" title="Logo"><img src="/images/G-shock logo.png" alt="Logo" /></Link></li>
                     </ul>
 
-                </ul>
-            </nav>
-        </header>
+                    <nav>
+
+                        <div className="menu" 
+                        onClick={() => {
+                            setMenuOpen(!menuOpen);
+                        }}>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
+
+                        <ul className={`Menus ${menuOpen ? "" : "open"}`}>
+                                <li><Link to="/">Home</Link></li>
+                                <li><Link to="/">About</Link></li>
+                                <li><Link to="/">Features</Link></li>
+                                <li><Link to="/">Contact</Link></li>
+
+                                <ul className="buttons">
+                                    <li><Link to="/login"><button className="button button-1">Login</button></Link></li>
+                                    <li><Link to="/sign-up"><button className="button button-2">Sign Up</button></Link></li>
+                                </ul>
+                            </ul>
+                    </nav>
+                </header>
+
+                <main>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/sign-up" element={<Signup />} />
+                    </Routes>
+                </main>
+            </>
+        </BrowserRouter>
     );
 }
 
-export default Menu; 
+export default Menu;
