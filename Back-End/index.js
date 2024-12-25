e0ef4a1316a22b75165b901bcef784176c74af7c
Mohamed Shakin <shakincc02@gmail.com>
2024-12-25 19:49:38 +0400

    updated the url

--- Back-End/index.js
+++ Back-End/index.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(cors(
     {
-        origin: [""],
+        origin: ["https://g-shock-watch-frontend.vercel.app/"],
         methods:["POST","GET"],
         credentials: true
     }
